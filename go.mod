ada28519c9122fcb7c1cfa8c153e455f5734f77b
Elliot Forbes <emforce@live.com>
2019-03-03 12:36:54 +0000

    initial commit

--- go.mod
+++
@@ -1,1 +0,0 @@
-module github.com/elliotforbes/youtube-stats
--- main.go
+++ main.go
@@ -2,19 +2,15 @@ package main
 
 import (
 	"fmt"
-	"html"
 	"log"
 	"net/http"
 )
 
 func main() {
+	fmt.Println("Go Docker Tutorial")
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
-	})
-
-	http.HandleFunc("/hi", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hi")
+		fmt.Fprintf(w, "Hello World")
 	})
 
 	log.Fatal(http.ListenAndServe(":8081", nil))
